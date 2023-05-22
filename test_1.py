42c5e81deb93e6412b46dd27be426448b5ad86fe
WildCat0U0 <2443133871@qq.com>
2023-05-22 13:45:50 +0800

    fuck ml

--- test_1.py
+++ test_1.py
@@ -1,31 +1,39 @@
-# 计算一阶矩，二阶矩，三阶矩
-import cv2
+# import the necessary packages
 import numpy as np
+import cv2
+import os
 import csv
-
-# heade# r = [['imgname',"b_mean","g_mean","r_mean", "b_2nd_moment", "g_2nd_moment", "r_2nd_moment", "b_3rd_moment", "g_3rd_moment", "r_3rd_moment"]]
-# with open('shuizhi.csv', mode='w', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerows(header)
-n=1
+n = 1
 for i in range(6):
-    path = "img/5_" + str(n) + ".jpg"
+    path = "img/5_"+str(n)+".jpg"
     img = cv2.imread(path)
-    # 分离通道
-    b, g, r = cv2.split(img)
-    # 计算矩
-    b_mean = np.mean(b)
-    g_mean = np.mean(g)
-    r_mean = np.mean(r)
-    b_2nd_moment = np.mean(np.square(b - b_mean))
-    g_2nd_moment = np.mean(np.square(g - g_mean))
-    r_2nd_moment = np.mean(np.square(r - r_mean))
-    b_3rd_moment = np.mean(np.power(b - b_mean, 3))
-    g_3rd_moment = np.mean(np.power(g - g_mean, 3))
-    r_3rd_moment = np.mean(np.power(r - r_mean, 3))
-    data = [[path,b_mean, g_mean, r_mean, b_2nd_moment, g_2nd_moment, r_2nd_moment, b_3rd_moment, g_3rd_moment, r_3rd_moment,5]]
-    with open("shuizhi.csv",mode="a", newline="")as file:
+    # Convert BGR to HSV colorspace
+    # Split the channels - h,s,v
+    b,g,r = cv2.split(img)
+    # Initialize the color feature
+    color_feature = []
+    # N = h.shape[0] * h.shape[1]
+    # The first central moment - average
+    b_mean = np.mean(b)/255 # np.sum(h)/float(N)
+    g_mean = np.mean(g)/255  # np.sum(s)/float(N)
+    r_mean = np.mean(r)/255  # np.sum(v)/float(N)
+    color_feature.extend([b_mean, g_mean, r_mean])
+    # The second central moment - standard deviation
+    b_std = np.std(b)/255  # np.sqrt(np.mean(abs(h - h.mean())**2))
+    g_std = np.std(g)/255  # np.sqrt(np.mean(abs(s - s.mean())**2))
+    r_std = np.std(r)/255  # np.sqrt(np.mean(abs(v - v.mean())**2))
+    color_feature.extend([b_std, g_std, r_std])
+    # The third central moment - the third root of the skewness
+    b_skewness = np.mean(abs(b - b.mean())**3)
+    g_skewness = np.mean(abs(g - g.mean())**3)
+    r_skewness = np.mean(abs(r - r.mean())**3)
+    b_thirdMoment = b_skewness**(1./3)
+    g_thirdMoment = g_skewness**(1./3)
+    r_thirdMoment = r_skewness**(1./3)
+    color_feature.extend([b_thirdMoment/255, g_thirdMoment/255, r_thirdMoment/255])
+    color_feature.extend([5])
+    with open("shuizhi.csv",mode="a",newline="")as file:
         writer = csv.writer(file)
-        writer.writerows(data)
-    n = n+1
-
+        writer.writerows([color_feature])
+    print("success" + str(n) + "file")
+    n = n + 1
